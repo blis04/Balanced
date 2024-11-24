4d4a1b7ee6d20d7c2dc308aced51b984525d74d8
Sam Gearou <sam.gearou@robinhood.com>
2024-11-24 14:49:16 -0800

    add basic unit test

--- src/SQLLiteDB.java
+++ src/SQLLiteDB.java
@@ -1,13 +1,12 @@
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLLiteDB {
 
-    public static void connect() {
-        // connection string
-        var url = "jdbc:sqlite:/Users/sam.gearou/sqlite/chinook.db";
-
+    public void connect(String url) {
         try (var conn = DriverManager.getConnection(url)) {
+            // TODO: implement what we want to do after connecting to the DB
             System.out.println("Connection to SQLite has been established.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
--- test/java/SQLLiteConnectionTest.java
+++ test/java/SQLLiteConnectionTest.java
@@ -1,4 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SQLLiteConnectionTest {
-    
-    
+
+    private static final String HOME_DIR = System.getenv("HOME");
+    private static final String DB_URL = String.format("jdbc:sqlite:/Users/%s/sqlite/chinook.db", HOME_DIRg);
+
+    @Test
+    public void testDatabaseConnection() {
+        SQLLiteDB db = new SQLLiteDB();
+
+        assertDoesNotThrow(() -> db.connect(DB_URL));
+    }
 }
